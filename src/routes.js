8c57f116558c1bcd1a4e9d5445bed00c9c7a09f1
debs <debfdias@gmail.com>
2020-09-10 13:18:25 -0300

    second fix

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,4 @@
 const routes = require("express").Router();
-const multer = require("multer");
-const multerConfig = require("./config/multer");
 
 const Post = require("./models/Post");
 
@@ -10,26 +8,12 @@ routes.get("/posts", async (req, res) => {
   return res.json(posts);
 });
 
-routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
-  const { originalname: name, size, key, location: url = "" } = req.file;
-
-  const post = await Post.create({
-    name,
-    size,
-    key,
-    url
-  });
-
-  return res.json(post);
+routes.get("/", async (req, res) => {
+  res.send('Hello World!')
 });
 
-routes.delete("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-
-  await post.remove();
-
-  return res.send();
+routes.get("/hey", async (req, res) => {
+  res.send('HEEEY')
 });
-
 
 module.exports = routes;
