3aca013b2790d42e18a031218666902d49547dfc
debs <debfdias@gmail.com>
2020-09-10 14:08:01 -0300

    rollback

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
 
+const s3 = new aws.S3();
+
 const PostSchema = new mongoose.Schema({
   name: String,
   size: Number,
@@ -15,5 +17,31 @@ const PostSchema = new mongoose.Schema({
   }
 });
 
+PostSchema.pre("save", function() {
+  if (!this.url) {
+    this.url = `${process.env.APP_URL}/files/${this.key}`;
+  }
+});
+
+PostSchema.pre("remove", function() {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key
+      })
+      .promise()
+      .then(response => {
+        console.log(response.status);
+      })
+      .catch(response => {
+        console.log(response.status);
+      });
+  } else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, "..", "..", "tmp", "uploads", this.key)
+    );
+  }
+});
 
 module.exports = mongoose.model("Post", PostSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,43 @@
 const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
+const Post = require("./models/Post");
 
-routes.get("/", async (req, res) => {
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.get("/hey", async (req, res) => {
   res.send('Hello World!')
 });
 
-routes.get("/heey", async (req, res) => {
-  res.send('HEEEY')
+routes.get("/", async (req, res) => {
+  res.send('running')
 });
+
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, key, location: url = "" } = req.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  });
+
+  return res.json(post);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
+});
+
 
 module.exports = routes;
